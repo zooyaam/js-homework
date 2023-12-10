ee382be37fbb2246a16060f6f23c8772da79e6e9
zooyaam <yjoo2498@naver.com>
2023-12-10 17:22:18 +0900

    정규표현식을 이용한 email, password의 invalid 작성

--- mission-01/naver_login/js/main.js
+++ mission-01/naver_login/js/main.js
@@ -1,36 +1,43 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+
+// querySelector
+const userEmail = document.querySelector("#userEmail");
+const userPassword = document.querySelector("#userPassword");
+const button = document.querySelector(".btn-login");
+
+// 1. email 정규표현식을 사용한 validation
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
 }
 
-/*
-
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
-
-*/
-
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(text).toLowerCase())
+function handleInputEmail() {
+  if (!emailReg(userEmail.value)) {
+    userEmail.classList.add("is--invalid");
+  } else {
+    userEmail.classList.remove("is--invalid");
+  }
 }
 
-function pwReg(text){
+userEmail.addEventListener("input", handleInputEmail);
+
+// 2. pw 정규표현식을 사용한 validation
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+function handleInputPw() {
+  if (!pwReg(userPassword.value)) {
+    userPassword.classList.add("is--invalid");
+  } else {
+    userPassword.classList.remove("is--invalid");
+  }
+}
 
-
-
-
-
-
-
-
-
-
+userPassword.addEventListener("input", handleInputPw);
